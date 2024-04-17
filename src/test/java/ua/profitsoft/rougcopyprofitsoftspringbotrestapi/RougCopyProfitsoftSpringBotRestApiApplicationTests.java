91952a304182cb5f009f521bb6d7e22c8fb91f31
Viacheslav Korbut <minowaman777@gmail.com>
2024-04-17 19:30:31 +0300

    fist commit

---
+++ src/test/java/ua/profitsoft/rougcopyprofitsoftspringbotrestapi/RougCopyProfitsoftSpringBotRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.profitsoft.rougcopyprofitsoftspringbotrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RougCopyProfitsoftSpringBotRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
